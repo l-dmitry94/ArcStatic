99b8f5f1f51e820c342c975785e28c29a456d558
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-08-14 23:48:26 +0200

    Hero updated

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -7,11 +7,11 @@ const Hero: React.FC = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.hero_container}>
-        <h1 className={styles.hero_title}>
-          EXPERT<br />IN NAVIGATION<br />WEB<br />DEVELOPMENT
-        </h1>
         <div className={styles.hero_text}>
           <p>Our web development team helps businesses grow and succeed in the online space by providing website design and maintenance,</p>
+          <h1 className={styles.hero_title}>
+            EXPERT<br />IN NAVIGATION<br />WEB<br />DEVELOPMENT
+          </h1>
           <p>UX/UI design and platform integration. We bring your ideas to life by providing high functionality, usability and state-of-the-art technology.</p>
         </div>
         <div className={styles.hero_buttonContainer}>
