91e87ac38548d186dac47289b02f5636741f49cb
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-08-16 11:54:13 +0300

    Revert "Update ContactsPage.tsx"
    This reverts commit fc4e9a8ed4b1a474f3da751603d6e73cc56acee1.

--- components/ContactsPage/ContactsPage.tsx
+++ components/ContactsPage/ContactsPage.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import styles from './contacts-page.module.scss';
 import Form from '@/components/ContactsPage/Form/Form';
 import icon from '../../public/icons/contact-icon.svg';
+import Image from 'next/image';
+import ContactsInfo from '@/components/ContactsPage/ContactsInfo/ContactsInfo';
 
 export interface ContactsPageProps {}
 
