21bfe413a7e51868796a7304c478e68df403e474
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-08-16 12:04:19 +0300

    Revert "Merge pull request #8 from l-dmitry94/Hero"
    This reverts commit 6607af6a2b21a1ced0c3a2ad6491043c7f6b099f, reversing
    changes made to adc06cafcc94cde91465c33e3113371f17fe6cf7.

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { FC, ReactNode } from 'react';
+import type { Metadata } from 'next';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import '../styles/global.scss';
+
+export const metadata: Metadata = {
+    title: 'ArcStatic',
+    description: 'Expert in navigation web development',
+};
+
+interface IRootLayout {
+    children: ReactNode;
+}
+
+const RootLayout: FC<IRootLayout> = ({ children }) => {
+    return (
+        <html lang="en">
+            <head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+            </head>
+            <body>
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
+};
+
+export default RootLayout;
