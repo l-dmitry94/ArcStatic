38c3af8b08fe7884d06d0adacfed4cf4703e000a
sivlvy <vladislavsidenko@ukr.net>
2024-08-19 22:02:18 +0300

    added TeamPage component

---
+++ components/TeamPage/helpers/team.js
@@ -0,0 +1,69 @@
+import dmytroImg from '../../../public/images/team/dmytroLiuba.png';
+
+export const data = [
+    {
+        id: 0,
+        name: 'Dmytro Liuba',
+        role: 'Team Lead Fullstack Developer',
+        description:
+            'Dmitry is our leading fullstack developer and team lead. He is a masterful project manager, combining frontend and backend development to create complex and efficient solutions. His experience and leadership skills help our team to achieve high results.',
+        image: dmytroImg,
+    },
+    {
+        id: 1,
+        name: 'LIUBOV POHUDINA',
+        role: 'FrontEnd Developer',
+        description:
+            'Skilled frontend developer, skilled in a wide range of technologies for both frontend and backend. Her strengths are analytical thinking and the ability to quickly adapt to new challenges. Love always strives to build quality, efficient and scalable websites, providing a great user experience.',
+        image: dmytroImg,
+    },
+    {
+        id: 2,
+        name: 'Vladyslav Sidenko',
+        role: 'FrontEnd Developer',
+        description:
+            'a frontend developer who is confident in modern technologies for creating complex websites. His competencies cover both frontend and backend development, which allows him to create complete and effective solutions. Vlad strives to constantly improve his skills and always finds the best ways to realize projects.',
+        image: dmytroImg,
+    },
+    {
+        id: 3,
+        name: 'POLINA LINING',
+        role: 'FrontEnd Developer',
+        description:
+            'A talented frontend developer with extensive experience in building dynamic and scalable websites. She has in-depth knowledge in both client and server-side development. Pauline always strives to implement the latest technologies and best practices, ensuring high quality and performance of the final product.',
+        image: dmytroImg,
+    },
+    {
+        id: 4,
+        name: 'DANILO KULAK',
+        role: 'FrontEnd Developer',
+        description:
+            'An experienced frontend developer with extensive knowledge and skills in web technologies. His approach to development is based on attention to detail and commitment to high quality. Danil confidently works with both client and server side, creating efficient and reliable solutions for our clients.',
+        image: dmytroImg,
+    },
+    {
+        id: 5,
+        name: 'PETRO BRODETSKYI',
+        role: 'BackEnd Developer',
+        description:
+            'Talented backend developer with deep knowledge and experience with both frontend and backend technologies. His professionalism and attention to detail help to create high-quality and reliable websites. Peter always strives for perfection in every project, finding optimal solutions for the most complex tasks.\n',
+        image: dmytroImg,
+    },
+
+    {
+        id: 6,
+        name: 'OLEKSII OREL',
+        role: 'BackEnd Developer',
+        description:
+            'is a frontend developer with deep knowledge and skills in web technologies. His approach to development is based on attention to detail and commitment to high quality. Alexey confidently works with both client and server side, creating efficient and reliable solutions for our clients.',
+        image: dmytroImg,
+    },
+    {
+        id: 7,
+        name: 'TETIANA VARZONOVA',
+        role: 'UI/UX Designer',
+        description:
+            'is a talented UI/UX designer who creates intuitive and visually appealing interfaces. She thoroughly researches user needs and applies best design practices to create user-friendly and effective products. Tatiana has a great sense of style and detail, which allows her to create designs that are not only attractive but also functional.',
+        image: dmytroImg,
+    },
+];
